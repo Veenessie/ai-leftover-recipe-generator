0cbb3eb1e87d713f61b900dc9a6e76acbfc514c3
Verity <mastersverity@gmail.com>
2024-04-14 14:54:11 +0100

    integrated axios and AI api

--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
-function generateRecipe(event) {
-  event.preventDefault();
+function showRecipe(response) {
+  console.log(response.data);
 
   new Typewriter("#recipe", {
-    strings: `  <h2>Easy kedgeree</h2>
-        <h3>Ingredients</h3>
-        <ul>
-          <li>2 tbsp curry powder of your choice (we used madras)</li>
-          <li>300g basmati rice</li>
-          <li>600ml chicken stock</li>
-          <li>400g skinless smoked haddock</li>
-          <li>100g frozen peas</li>
-        </ul>
-        <h3>Method</h3>
-        <ul>
-          <li>
-            Heat the oven to 180C/160C fan/gas 4. Heat an ovenproof saucepan or
-            flameproof casserole over a medium heat and toast the curry powder
-            for 1 min. Stir in the rice to coat it in the curry powder, then
-            pour over the stock. Bring to the boil, then lay the haddock on top.
-            Cover. Cook in the oven for 30 mins.
-          </li>
-          <li>
-            Carefully remove from the oven. Leave to rest for a minute, then
-            stir through the peas while breaking up the haddock and fluffing the
-            rice. Season to taste, then serve.
-          </li>
-        </ul>`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: null,
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let userInstructions = document.querySelector("#user-instructions");
+
+  let apiKey = `7b12a4c8c3fba7979089o3b3ff6teab1`;
+  let prompt = `Generate a recipe using these leftovers - ${userInstructions.value}`;
+  let context = `You're a helpful AI assistant who helps create recipes for people with their leftovers. Please keep recipes to less than 7 ingredients and 30mins or under cooking time. Please return your answer in html format.`;
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(showRecipe);
 }
 
 let formElement = document.querySelector("#recipe-form");
